d90073f37dd4661387c7c801582c64840a1672d1
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-04-08 11:11:38 +0530

    CURD Operation Done In NestJS With MongoDb

--- demo-nest/src/app.module.ts
+++ demo-nest/src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import configuration from './config/configuration';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './modules/users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [configuration]
-  }), AuthModule,DatabaseModule
+  }), AuthModule,DatabaseModule,UserModule
 ],
   controllers: [],
   providers: [],
