50db61be2d0e08dfabc7c178d9b71992456f332f
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-04-08 11:30:50 +0530

    some bug fixes in nestWithMongo

--- demo-nest/src/modules/auth/auth.module.ts
+++ demo-nest/src/modules/auth/auth.module.ts
@@ -1,25 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { CommonModule } from 'src/common/guards/common.module';
 
 @Module({
   imports: [ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
-      }),
-    }),
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
-    ])
+    ]),CommonModule
   ],
   controllers: [AuthController],
   providers: [AuthService, AuthGuard]
